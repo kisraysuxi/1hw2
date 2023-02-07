85d65333c82da46e914210842ba779d0b4ca110e
kisraysuxi <elinatadu@icloud.com>
2023-02-07 16:40:19 +0600

    hw

--- 1hw2/Card.swift
+++ 1hw2/Card.swift
@@ -1,17 +1,24 @@
-import Foundation
 //Cоздать класс Client c параметрами firstname, lastname. Cоздать класс Card c параметрами bankName, cardNumber и bill. Добавить в класс Client еще один параметр cards: [Card]. Cоздать класс Bank, в котором имеется функция перевода или оплаты денег. Функция должна запрашивать "Выберите карту", создается поле для ввода номера карты, после чего она должна распознать имя банка и запринтить в консоль. Затем запрашивается номер счета другого Сlient, вы вводите номер карты, в консоль должна отобразиться ФИО клиента. После чего запрашивается сумма и происходит перевод денег. В main cоздать объекты, заполнить данными и произвести перевод.
 
-print("Выберите карту:")
+import Foundation
 
-var client1 = Client(firstName: "Meder", lastName: "Ariev")
-var card1 = Card(bankName: "Optima", cardNumber: 545255, bill: 40000)
-card1.showCard(client1: card1, client2: card2, amount: 2000)
-client1.showInfo()
+class Card{
+    var bankName: String
+    var cardNumber: Int
+    var bill: Int
 
-print("Выберите карту:")
-var client2 = Client(firstName: "Askar", lastName: "Bayazov")
-var card2 = Card(bankName: "Visa", cardNumber: 4342425, bill: 45000)
-card2.showCard(client1: card1, client2: card2, amount: 2000)
-client2.showInfo()
+    init(bankName: String, cardNumber: Int, bill: Int) {
+        self.bankName = readLine()!
+        self.cardNumber = cardNumber
+        self.bill = bill
+    }
+    
+    func showCard(client1: Card, client2: Card, amount: Int) {
+            print("Ваша карта: \n \(bankName)")
+            print("Ваш номер карты: \n \(cardNumber)")
+                    
+}
+
+    }
 
 
--- 1hw2/Client.swift
+++ 1hw2/Client.swift
@@ -1,17 +1,19 @@
-import Foundation
 //Cоздать класс Client c параметрами firstname, lastname. Cоздать класс Card c параметрами bankName, cardNumber и bill. Добавить в класс Client еще один параметр cards: [Card]. Cоздать класс Bank, в котором имеется функция перевода или оплаты денег. Функция должна запрашивать "Выберите карту", создается поле для ввода номера карты, после чего она должна распознать имя банка и запринтить в консоль. Затем запрашивается номер счета другого Сlient, вы вводите номер карты, в консоль должна отобразиться ФИО клиента. После чего запрашивается сумма и происходит перевод денег. В main cоздать объекты, заполнить данными и произвести перевод.
 
-print("Выберите карту:")
+import Foundation
 
-var client1 = Client(firstName: "Meder", lastName: "Ariev")
-var card1 = Card(bankName: "Optima", cardNumber: 545255, bill: 40000)
-card1.showCard(client1: card1, client2: card2, amount: 2000)
-client1.showInfo()
-
-print("Выберите карту:")
-var client2 = Client(firstName: "Askar", lastName: "Bayazov")
-var card2 = Card(bankName: "Visa", cardNumber: 4342425, bill: 45000)
-card2.showCard(client1: card1, client2: card2, amount: 2000)
-client2.showInfo()
-
-
+class Client{
+    var firstName: String
+    var lastName: String
+    var cards: [Card] = []
+    
+    init(firstName: String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+    
+    func showInfo(){
+        print("name: \(firstName), last name: \(lastName)")
+        
+    }
+}
